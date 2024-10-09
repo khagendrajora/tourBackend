52e3bc98c509c0189791357bbd7af9b909308637
khagendrajora <khagijora2074@gmail.com>
2024-10-09 16:17:07 +0545

    admin dashboard completed

--- src/routes/Pages/LandingPageRoute.ts
+++ src/routes/Pages/LandingPageRoute.ts
@@ -16,40 +16,40 @@ import upload from "../../middleware/fileUpload";
 const router = express.Router();
 
 router.post(
-  "addhero",
+  "/addhero",
   upload.fields([{ name: "hero_image", maxCount: 10 }]),
   addHero
 );
-router.get("gethero", getHero);
+router.get("/gethero", getHero);
 
-router.post("addaboutus", addAboutUs);
+router.post("/addaboutus", addAboutUs);
 
-router.get("addaboutus", addAboutUs);
+router.get("/addaboutus", addAboutUs);
 
-router.put("updateaboutus/:id", updateAboutUS);
+router.put("/updateaboutus/:id", updateAboutUS);
 
 router.post(
-  "addblogs",
+  "/addblogs",
   upload.fields([{ name: "blogs_image", maxCount: 10 }]),
   addBlogs
 );
 
-router.get("getblogs", getBlogs);
+router.get("/getblogs", getBlogs);
 
-router.put("updateblogs/:id", updateBlogs);
+router.put("/updateblogs/:id", updateBlogs);
 
 router.post(
-  "addDest",
+  "/addDest",
   upload.fields([{ name: "dest_image", maxCount: 10 }]),
   addDest
 );
 
 router.get(
-  "getDest",
+  "/getDest",
 
   getDest
 );
 
-router.put("updateDest/:id", updateDest);
+router.put("/updateDest/:id", updateDest);
 
 export default router;
